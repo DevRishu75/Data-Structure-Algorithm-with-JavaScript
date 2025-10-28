77e773e6facbf6bfd47bf4bf44618f717697f7c7
DevopsRishu <devopsrishabh77@gmail.com>
2025-10-28 14:44:55 +0530

    New file DSA

---
+++ DSA1.js
@@ -0,0 +1,22 @@
+// a = 12;
+// console.log(a)
+// var a;
+// we can even use var variable before intialization // but we use let variable
+// let a = 5
+// let b = 8
+// sum = a+b;
+// console.log(sum)
+// let a = 12;
+// let b = "13"
+// console.log(a+b)
+// console.log(typeof(a+b)) //string
+// let c= 1.23
+// console.log((a+b+c))
+// console.log(typeof(a+b+c))//string 
+let a = 10;
+let b = 20;
+console.log("sum of 10 and 20"+a+b) // string 
+// but we need to add a+b so we use BODMAS
+console.log("sum of 10 and 20 is "+(a+b)) // string sum of 10 and 20 is 30
+console.log(a+b+"is sum of 10 and 20") // string // 30 is sum of 10 and 20
+console.log("1"+1)
