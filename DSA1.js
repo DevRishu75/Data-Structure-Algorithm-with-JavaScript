88b0d6556c41ed1db81aad3145a368e19acd8f8b
DevopsRishu <devopsrishabh77@gmail.com>
2025-10-28 16:15:45 +0530

    Update

--- DSA1.js
+++ DSA1.js
@@ -52,3 +52,61 @@
 // b = a-b; // 30 - 20 = 10
 // a = a-b;
 // console.log(a,b); // a = 20,b = 10
+
+// third methods
+// let a = 10;
+// let b = 20;
+// [a,b] = [b,a]
+
+// console.log(a,b)
+// let a = 112;
+// let b = 22;
+// // console.log(Math.floor(a/b))\ we alway math.floor because we are using Javascript as our language/
+// console.log(b%a)//  if divdend is smaller than divisor we should not divide it but the remaider wil be the divdend 22
+// let a = 4567;
+// // sum of digit in a number 
+// console.log(a%10); // we wil be getting the last number as remainder 
+// console.log(Math.floor(a/10)) // we will the starting 3 digits of number
+// relation operators
+console.log(10>=10)// we take it as or means 10 is greater or less than 10
+console.log(10<=7) 
+console.log(10!=11)
+ 
+// post Increment // pre Increment
+// Post Increment
+// let a = 10;
+// let b = a++;
+// console.log(a) // a = 11 // firstly a  will be used  so a  = 11
+// console.log(b) // b = 10
+// // Pre Increment
+// let  p = 10;
+// let q = ++p;
+// console.log(p)//11
+// console.log(q)//11
+//Post decrement
+// let i = 11;
+// i = i++ + ++i; // first post increment i = 11 and then i becomes 12 and then pre incremnet so i become 13 and then plus so finally i = 11 + 13 = 24
+// console.log(i) // 24
+// // Question
+// let a = 11, b = 22;
+// let c = a + b + a++ + b++ + ++a + ++b;
+// console.log("a=" + a)//13
+// console.log("b=" +b)//24
+// console.log("c=" +c)//103
+
+// let b = true; // here b = 1 
+// b++;
+// console.log(b) // here b = 2
+// // same goes for false
+// let a = false;
+// a++;
+// console.log(a)// here false means 0 so a++ wil 1 now
+
+
+// let a = 10++;// this gives an error because we can't give a number ++ operator
+
+// console.log(a)/
+let i = 11;
+let b = ++(a++)
+console.log(b)
+
