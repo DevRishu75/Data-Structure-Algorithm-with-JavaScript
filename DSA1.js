fefa1b8f2b4494679e4859be0dc4dc2b8af38af9
DevopsRishu <devopsrishabh77@gmail.com>
2025-10-28 15:01:55 +0530

    New Html File

--- DSA1.js
+++ DSA1.js
@@ -13,14 +13,20 @@
 // let c= 1.23
 // console.log((a+b+c))
 // console.log(typeof(a+b+c))//string 
-let a = 10;
-let b = 20;
-console.log("sum of 10 and 20"+a+b) // string 
-// but we need to add a+b so we use BODMAS
-console.log("sum of 10 and 20 is "+(a+b)) // string sum of 10 and 20 is 30
-console.log(a+b+"is sum of 10 and 20") // string // 30 is sum of 10 and 20
-console.log("1"/1);//number 1
-console.log("1"*1);//number 1
-console.log("1"-1);//number 1
-// here javascript converts "1" to a number type 1
-console.log("1"%1) // number 0
+// let a = 10;
+// let b = 20;
+// console.log("sum of 10 and 20"+a+b) // string 
+// // but we need to add a+b so we use BODMAS
+// console.log("sum of 10 and 20 is "+(a+b)) // string sum of 10 and 20 is 30
+// console.log(a+b+"is sum of 10 and 20") // string // 30 is sum of 10 and 20
+// console.log("1"/1);//number 1
+// console.log("1"*1);//number 1
+// console.log("1"-1);//number 1
+// // here javascript converts "1" to a number type 1
+// console.log("1"%1) // number 0
+// let age = prompt("What is you age")
+// age = Number(age);
+// console.log(age)
+// console.log(typeof age); // prompt default takes the input in string and if we need it in number 
+let age = Number(prompt("Enter your age ")); // Number is a parameter and prompt is a function
+console.log(age);
