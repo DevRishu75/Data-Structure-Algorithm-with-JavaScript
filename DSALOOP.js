99e32d1461795297fbe2581fff1ee077b982b167
DevopsRishu <devopsrishabh77@gmail.com>
2025-11-14 09:40:34 +0530

    ne'

--- DSALOOP.js
+++ DSALOOP.js
@@ -253,20 +253,20 @@ let n = Number(prompt("Enter a number "));
 // }
 // Hollow pyramid//
 
-for(let i = 1; i<n;i++){
-    for(let j=n;j>i;j--){
-        process.stdout.write(" ")
-    }
-    for(let j=1;j<=2*i-1;j++){
-        if(j===1||j===2*i-1){
-            process.stdout.write("*")
-        }
-        else{
-            process.stdout.write(" ")
-        }
-    }
-    console.log()
-}
-for(let i = 1; i<2*n;i++){
-    process.stdout.write("*")
-}
+// for(let i = 1; i<n;i++){
+//     for(let j=n;j>i;j--){
+//         process.stdout.write(" ")
+//     }
+//     for(let j=1;j<=2*i-1;j++){
+//         if(j===1||j===2*i-1){
+//             process.stdout.write("*")
+//         }
+//         else{
+//             process.stdout.write(" ")
+//         }
+//     }
+//     console.log()
+// }
+// for(let i = 1; i<2*n;i++){
+//     process.stdout.write("*")
+// }
